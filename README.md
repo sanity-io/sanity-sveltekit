7ecfd9b14709df32ea009bf58ac04274eeec9407
Rupert Dunk <rupert@rupertdunk.com>
2025-10-02 12:52:12 +0100

    refactor: update documentation, simplify test app studio config (#107)

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# The Official Sanity integration for SvelteKit
-
-## Installation
-
-```bash
-npm install @sanity/sveltekit
-```
+./packages/sanity-sveltekit/README.md
--- apps/test/src/routes/studio/[...catchall]/+page.svelte
+++ apps/test/src/routes/studio/[...catchall]/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
   import config from '$lib/sanity/sanity.config';
   import { SanityStudio } from '@sanity/sveltekit';
-  import {
-    PUBLIC_SANITY_PROJECT_ID as projectId,
-    PUBLIC_SANITY_DATASET as dataset
-  } from '$env/static/public';
 </script>
 
-<SanityStudio config={{ ...config, projectId, dataset }} />
+<SanityStudio {config} />
